81a524b54942b0e569a2269cf7f84679ebd48543
ASIF-I-KHAN <asifkhan.9781@gmail.com>
2024-05-30 21:14:30 +0530

    adding ports and activity service

--- ActivityService/src/main/java/com/goTravel/activityService/ActivityServiceApplication.java
+++ ActivityService/src/main/java/com/goTravel/activityService/ActivityServiceApplication.java
@@ -2,12 +2,21 @@ package com.goTravel.activityService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ActivityServiceApplication {
+
+	@GetMapping("/api3")
+	public String getMS1() {
+		return "Activityservice up";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ActivityServiceApplication.class, args);
+		System.out.println("Activity service");
 	}
 
 }
